30dc98fd9caf25e6a20b06636922ca3246748df3
Victor Oliveira Almeida <victor.almeida.ti.celular@gmail.com>
2025-03-31 12:04:03 -0300

    ready

--- src/env.ts
+++ src/env.ts
@@ -1,19 +1,38 @@
+
+import { createEnv } from "@t3-oss/env-nextjs";
 import { z } from "zod";
 
-const envSchema = z.object({
-  NEXT_PUBLIC_API_BASE_URL: z.string().url(),
-  APP_URL: z.string().url(),
-});
+export const env = createEnv({
+  server: {
+    APP_URL: z.string().url(),
+  },
 
-const parseEnv = envSchema.safeParse(process.env);
+  client: {
+    NEXT_PUBLIC_API_BASE_URL: z.string().url(),
+  },
 
-if (!parseEnv.success) {
-  console.error(
-    "Invalid environment variables",
-    parseEnv.error.flatten().fieldErrors,
-  );
+  runtimeEnv: {
+    APP_URL: process.env.APP_URL,
+    NEXT_PUBLIC_API_BASE_URL: process.env.NEXT_PUBLIC_API_BASE_URL,
+  },
+})
 
-  throw new Error("Invalid environment variables");
-}
+// import { z } from "zod";
 
-export const env = parseEnv.data;
+// const envSchema = z.object({
+//   NEXT_PUBLIC_API_BASE_URL: z.string().url(),
+//   APP_URL: z.string().url(),
+// });
+
+// const parseEnv = envSchema.safeParse(process.env);
+
+// if (!parseEnv.success) {
+//   console.error(
+//     "Invalid environment variables",
+//     parseEnv.error.flatten().fieldErrors,
+//   );
+
+//   throw new Error("Invalid environment variables");
+// }
+
+// export const env = parseEnv.data;
